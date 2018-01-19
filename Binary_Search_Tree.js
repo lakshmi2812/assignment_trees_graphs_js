31ff2cf511266f554717c7214099e358255546ff
Austin1780 <austinwilliamsmith@gmail.com>
2018-01-19 10:49:32 -0700

    finished binary search tree

--- Binary_Search_Tree.js
+++ Binary_Search_Tree.js
@@ -1,6 +1,3 @@
-//Sample data
-//[8, 10, 3, 1, 6, 6, 14, 4, 7, 13]
-
 class BinaryNode {
   constructor(left, right, data) {
     this.left = left;
@@ -12,13 +9,14 @@ class BinaryNode {
 class BinarySearchTree {
   constructor(arr) {
     this.rootNode = new BinaryNode(null, null, arr[0]);
+    arr = arr.slice(1);
     arr.forEach(num => {
       this.addNode(new BinaryNode(null, null, num), this.rootNode);
     });
   }
 
   //add_node
-  addNode(node, parent) {
+  addNode(node, parent = this.rootNode) {
     if (node.data >= parent.data) {
       if (parent.right) {
         this.addNode(node, parent.right);
@@ -33,9 +31,25 @@ class BinarySearchTree {
       }
     }
   }
+
+  printTree(currentNode = this.rootNode) {
+    console.log(`Root Node: ${currentNode.data}`);
+    if (currentNode.left === null && currentNode.right === null) {
+      return;
+    }
+    if (currentNode.right) {
+      console.log(`Right: ${currentNode.right.data}`);
+      this.printTree(currentNode.right);
+    }
+    if (currentNode.left) {
+      console.log(`Left: ${currentNode.left.data}`);
+      this.printTree(currentNode.left);
+    }
+  }
 }
 
 //Testing
 
 let binaryTree = new BinarySearchTree([8, 10, 3, 1, 6, 6, 14, 4, 7, 13]);
 console.log(binaryTree);
+binaryTree.printTree();
